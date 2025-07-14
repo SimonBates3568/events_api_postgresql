f00f18c6e77162006a9e3f67d65194157bfeb280
Simon Bates <149606567+SimonBates3568@users.noreply.github.com>
2025-07-14 14:04:55 +0200

    Delete src directory

--- src/services/events/deleteEventById.js
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
-
-export default async function deleteEventById(id) {
-  return prisma.event.delete({
-    where: { id: Number(id) }, // Convert id to number here
-  });
-}
